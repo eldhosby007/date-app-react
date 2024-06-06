a0b16ac15cca6d550b78a299f0b2e86464e2ba6c
eldhosby007 <eldhosby007@gmail.com>
2024-06-06 23:13:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(step);
+  const divStyle = {
+    margin: "auto",
+    width: "500px",
+    padding: "15px",
+    alignItems: "center",
+  };
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  //const buttonStyle = { marginRight: "10px", display: "inline-block" };
+  function handleReset() {
+    setStep(0);
+    setCount(0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={divStyle}>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(event) => setStep(event.target.valueAsNumber)}
+        ></input>
+        <span>{step}</span>
+      </div>
+      {/* <div>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <span>
+          {"  "}step:{step}
+        </span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div> */}
+      <div>
+        <button onClick={() => setCount((s) => s - step)}>-</button>
+        <input type="text" readOnly value={count}></input>
+        <button onClick={() => setCount((s) => s + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
-
-export default App;
